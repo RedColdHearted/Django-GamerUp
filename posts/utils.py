8ede819e15f2cd9b1fe77327e8d41a5f642c204a
максим <maximovichpro@gmail.com>
2024-06-19 22:20:53 +0500

    additional utils file

---
+++ posts/utils.py
@@ -0,0 +1,20 @@
+import os
+import uuid
+
+
+def get_profile_image_upload_path(instance, filename: str) -> str:
+    """
+    This function is creating unique path name for profile pic file
+    """
+    ext = filename.split('.')[-1]
+    new_filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('profile_images', new_filename)
+
+
+def get_post_image_upload_path(instance, filename: str) -> str:
+    """
+    This function is creating unique path name for post pic file
+    """
+    ext = filename.split('.')[-1]
+    new_filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('profile_images', new_filename)
