502f9f0efa7cf014e8a0a7b0624b2c05a29a0fd6
максим <maximovichpro@gmail.com>
2024-06-19 22:19:28 +0500

    update views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework import viewsets, status, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
-# Create your views here.
+from accounts.serializers import CustomUserCreateSerializer
+from posts.serializers import ProfilePicSerializer, PostSerializer, PostPicSerializer, PostCommentSerializer
+from posts.models import UserAccount, ProfilePic, Post, PostPic, PostComment
+
+
+class UserViewSet(mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = UserAccount.objects.all()
+    serializer_class = CustomUserCreateSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class ProfilePicViewSet(viewsets.ModelViewSet):
+    queryset = ProfilePic.objects.all()
+    serializer_class = ProfilePicSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class PostViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        post_data = request.data
+        user = post_data['user']
+
+        serializer = PostSerializer(data=post_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'detail': 'пост создан'}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        post_data = request.data
+        user = post_data['user']
+        serializer = PostSerializer(data=post_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, *args, **kwargs):
+        post_data = request.data
+        user = post_data['user']
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['get'], detail=True)
+    def filter(self, request, pk=None):
+        posts = Post.objects.filter(user=pk)
+        return Response(PostSerializer(posts, many=True).data)
+
+
+class PostPicViewSet(viewsets.ModelViewSet):
+    queryset = PostPic.objects.all()
+    serializer_class = PostPicSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class PostCommentViewSet(viewsets.ModelViewSet):
+    queryset = PostComment.objects.all()
+    serializer_class = PostCommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    @action(methods=['get'], detail=True)
+    def filter(self, request, pk=None):
+        posts = PostComment.objects.filter(post=pk)
+        return Response(PostCommentSerializer(posts, many=True).data)
+
