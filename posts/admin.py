7a92035bcd0f50847c0ca334e4115693579ffd0a
максим <maximovichpro@gmail.com>
2024-06-21 00:08:47 +0500

    new models for admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import ProfilePic, Post, PostPic, PostComment
+
+admin.site.register(ProfilePic)
+admin.site.register(Post)
+admin.site.register(PostPic)
+admin.site.register(PostComment)
