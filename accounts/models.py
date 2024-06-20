1d787d8b2b410a5c5c39e6246885bdec38cf7af6
максим <maximovichpro@gmail.com>
2024-06-21 00:08:11 +0500

    update is_active at UserAccount model

--- accounts/models.py
+++ accounts/models.py
@@ -3,44 +3,43 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserM
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, username, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, username=username)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, name, password=None):
+    def create_superuser(self, email, username, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, username=username)
         user.set_password(password)
         user.is_superuser = True
         user.is_staff = True
+        # user.is_active = True
         user.save()
 
         return user
 
 
 class UserAccount(AbstractUser, PermissionsMixin):
-    username = None
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['username']
 
     def get_full_name(self):
         return self.name
