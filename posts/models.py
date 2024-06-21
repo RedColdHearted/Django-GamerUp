81f891355995608cba132cc67f618251409c6aa3
максим <maximovichpro@gmail.com>
2024-06-21 18:34:54 +0500

    many-to-many likes field for PostComment model

--- posts/models.py
+++ posts/models.py
@@ -37,11 +37,12 @@ class PostPic(models.Model):
 
 class PostComment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user = ForeignKey(UserAccount, on_delete=models.CASCADE, null=False, blank=True)
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, null=False, blank=True)
     user_post = models.ForeignKey(Post, on_delete=models.CASCADE, null=False, blank=True)
     created_at = models.DateField(auto_now_add=True)
     content = models.TextField()
     like_counter = models.IntegerField(default=0)
+    liked_by = models.ManyToManyField(UserAccount, related_name='liked_comments', blank=True)
 
     def __str__(self):
         return f"PostComment({self.id})"
