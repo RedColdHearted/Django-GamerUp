1e51cf9c828f1612b234cd1f063d6e30bf582572
максим <maximovichpro@gmail.com>
2024-06-22 20:05:45 +0500

    add new model to admin page

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from posts.models import ProfilePic, Post, PostPic, PostComment
+from posts.models import Post, PostPic, PostComment
 
-admin.site.register(ProfilePic)
 admin.site.register(Post)
 admin.site.register(PostPic)
 admin.site.register(PostComment)
