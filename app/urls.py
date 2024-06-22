b18c247f4dcd9b542101d8ae7bc9014da06caad3
максим <maximovichpro@gmail.com>
2024-06-23 00:37:21 +0500

    update posts.urls

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
-    path('api/v1', include('posts.urls')),
+    path('api/v1/', include('posts.urls')),
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
