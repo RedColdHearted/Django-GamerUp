082fd26e56388f8bf7d40cd57e1cede065ba4db8
максим <maximovichpro@gmail.com>
2024-06-23 00:38:53 +0500

    update has_object_permission

--- app/permissions.py
+++ app/permissions.py
@@ -8,4 +8,4 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.user == request.user
