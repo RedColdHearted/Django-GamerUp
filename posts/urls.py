d52a5802f6cbd604671a9234fc12ae921d477284
максим <maximovichpro@gmail.com>
2024-06-22 20:07:47 +0500

    update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,10 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from posts.views import UserViewSet, ProfilePicViewSet, PostViewSet, PostPicViewSet, PostCommentViewSet
+from posts.views import PostViewSet, PostPicViewSet, PostCommentViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'profiles', ProfilePicViewSet)
 router.register(r'posts', PostViewSet)
 router.register(r'post-pics', PostPicViewSet)
 router.register(r'post-comment', PostCommentViewSet)
