3203ca0e7f9394dd7523150627b1eec7e8485620
максим <maximovichpro@gmail.com>
2024-06-25 00:17:10 +0500

    refactor test cases

--- posts/tests/test_posts.py
+++ posts/tests/test_posts.py
@@ -1,7 +1,7 @@
-from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.urls import reverse
 
 from accounts.models import UserAccount
 from posts.models import Post
@@ -10,6 +10,7 @@ from posts.serializers import PostSerializer
 
 class PostViewSetTests(APITestCase):
     def setUp(self):
+        """set up for every test"""
         self.user1 = UserAccount(
             username='username1',
             email='a@a.com',
@@ -41,6 +42,7 @@ class PostViewSetTests(APITestCase):
             views=321,
             like_counter=321,
         )
+
         self.list_url = reverse('posts-list')
         self.post1_detail_url = reverse('posts-detail', kwargs={'pk': self.post1.pk})
         self.post2_detail_url = reverse('posts-detail', kwargs={'pk': self.post2.pk})
@@ -48,55 +50,33 @@ class PostViewSetTests(APITestCase):
         self.get_by_user2_url = reverse('posts-get-by-user', kwargs={'pk': self.user2.pk})
         self.like1_url = reverse('posts-like', kwargs={'pk': self.post1.pk})
         self.like2_url = reverse('posts-like', kwargs={'pk': self.post2.pk})
-        self.token1 = self.get_jwt_token_for_user(self.user1)
-        self.token2 = self.get_jwt_token_for_user(self.user2)
-
-    @staticmethod
-    def get_jwt_token_for_user(user):
-        refresh = RefreshToken.for_user(user)
-        return str(refresh.access_token)
-
-    def api_authentication(self, token):
-        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
-
-    def like_post(self, token, like_url, total_likes, post, unlike=False):
-        self.api_authentication(token)
-        response = self.client.post(like_url)
-        message = 'like removed' if unlike else 'liked'
-        like_counter = total_likes - 1 if unlike else total_likes + 1
-        self.assertEqual(response.data['status'], 'success')
-        self.assertEqual(response.data['message'], message)
-        self.assertEqual(response.data['like_counter'], like_counter)
-        post.refresh_from_db()
-
-    def complete_like_post(self, token, user, like_url, post, ):
-        self.like_post(token, like_url, post.like_counter, post)
-        self.assertTrue(post.liked_by.filter(pk=user.pk).exists())
-        self.assertIn(user, post.liked_by.all())
-        self.like_post(token, like_url, post.like_counter, post, unlike=True)
-        self.assertFalse(post.liked_by.filter(pk=user.pk).exists())
-        self.assertNotIn(user, post.liked_by.all())
+        self.token1 = self.user1.get_jwt_token_for_user()
+        self.token2 = self.user2.get_jwt_token_for_user()
 
     def test_list_posts(self):
+        """test: get all posts"""
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 4)
 
     def test_detail_post(self):
+        """test: get post1 by id"""
         response = self.client.get(self.post1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertDictEqual(response.data, PostSerializer(self.post1).data)
 
     def test_create_post(self):
-        self.api_authentication(self.token1)
+        """test: creating post as user1"""
+        self.user1.in_test_api_auth(self.client, self.token1)
         data = {'user': 1, 'title': 'some_title', 'content': 'some_content'}
         response = self.client.post(self.list_url, data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 3)
 
     def test_update_post(self):
+        """test: updating user's post"""
         # user1
-        self.api_authentication(self.token1)
+        self.user1.in_test_api_auth(self.client, self.token1)
         data = {'user': self.user1.id, 'title': 'updated title', 'content': 'updated content'}
         response = self.client.put(self.post1_detail_url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -105,25 +85,27 @@ class PostViewSetTests(APITestCase):
         self.assertEqual(self.post1.content, data['content'])
 
         # user2
-        self.api_authentication(self.token2)
+        self.user2.in_test_api_auth(self.client, self.token2)
         data = {'user': self.user2.id, 'title': 'updated title', 'content': 'updated content'}
         response = self.client.put(self.post1_detail_url, data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_posts(self):
+        """test: deleting user's post"""
         # user2
-        self.api_authentication(self.token2)
+        self.user2.in_test_api_auth(self.client, self.token2)
         response = self.client.delete(self.post1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(Post.objects.count(), 2)
 
         # user1
-        self.api_authentication(self.token1)
+        self.user1.in_test_api_auth(self.client, self.token1)
         response = self.client.delete(self.post1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Post.objects.count(), 1)
 
     def test_get_by_user_posts(self):
+        """test: get all user's posts"""
         # user1
         response = self.client.get(self.get_by_user1_url)
         self.assertEqual(len(response.data), 1)
@@ -135,30 +117,18 @@ class PostViewSetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_like_posts(self):
-        # post1
-        self.complete_like_post(self.token1, self.user1, self.like1_url, self.post1)
-        self.complete_like_post(self.token2, self.user2, self.like1_url, self.post1)
-
-        # post2
-        self.complete_like_post(self.token1, self.user1, self.like2_url, self.post2)
-        self.complete_like_post(self.token2, self.user2, self.like2_url, self.post2)
-
-        # user1, user2 in many-to-many field
-
-        self.api_authentication(self.token1)
+        """test: post liking"""
+        # user1
+        self.user1.in_test_api_auth(self.client, self.token1)
         response = self.client.post(self.like1_url)
         self.post1.refresh_from_db()
-        self.api_authentication(self.token2)
+        self.assertEqual(response.data['like_counter'], self.post1.like_counter)
+
+        # user2
+        self.user2.in_test_api_auth(self.client, self.token2)
         response = self.client.post(self.like1_url)
         self.post1.refresh_from_db()
+        self.assertEqual(response.data['like_counter'], self.post1.like_counter)
+
         self.assertIn(self.user1, self.post1.liked_by.all())
         self.assertIn(self.user2, self.post1.liked_by.all())
-
-        self.api_authentication(self.token1)
-        response = self.client.post(self.like1_url)
-        self.post1.refresh_from_db()
-        self.api_authentication(self.token2)
-        response = self.client.post(self.like1_url)
-        self.post1.refresh_from_db()
-        self.assertNotIn(self.user1, self.post1.liked_by.all())
-        self.assertNotIn(self.user2, self.post1.liked_by.all())
