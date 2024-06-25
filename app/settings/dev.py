92b944abb9fe5d7900c225f6ee7ab6ce30528749
максим <maximovichpro@gmail.com>
2024-06-25 20:04:47 +0500

    update dev settings config

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -3,4 +3,4 @@ from app.settings.base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(minutes=60)
+SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(hours=999)
