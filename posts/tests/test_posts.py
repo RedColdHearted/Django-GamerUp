1d09da1394f2ef65b9a946dcb96a45e65ccff731
максим <maximovichpro@gmail.com>
2024-06-26 02:16:22 +0500

    update PostViewSet tests

--- posts/tests/test_posts.py
+++ posts/tests/test_posts.py
@@ -3,44 +3,17 @@ from rest_framework.test import APITestCase
 
 from django.urls import reverse
 
-from accounts.models import UserAccount
 from posts.models import Post
 from posts.serializers import PostSerializer
+from posts.tests.setup_fabric import SetUpFabric
 
 
-class PostViewSetTests(APITestCase):
+class PostViewSetTests(APITestCase, SetUpFabric):
     def setUp(self):
         """set up for every test"""
-        self.user1 = UserAccount(
-            username='username1',
-            email='a@a.com',
-            name='name1',
-            password='password123',
-            is_active=True,
-        )
-        self.user2 = UserAccount(
-            username='username2',
-            email='b@b.com',
-            name='name2',
-            password='password123',
-            is_active=True,
-        )
-        self.user1.save()
-        self.user2.save()
-        self.post1 = Post.objects.create(
-            user=self.user1,
-            title='title',
-            content='content',
-            views=0,
-            like_counter=123,
-        )
-        self.post2 = Post.objects.create(
-            user=self.user2,
-            title='testing',
-            content='testing',
-            views=0,
-            like_counter=321,
-        )
+        self.setup_users()
+        self.setup_posts()
+        self.setup_tokens()
 
         self.list_url = reverse('posts-list')
         self.post1_detail_url = reverse('posts-detail', kwargs={'pk': self.post1.pk})
@@ -51,22 +24,20 @@ class PostViewSetTests(APITestCase):
         self.like2_url = reverse('posts-like', kwargs={'pk': self.post2.pk})
         self.views1_url = reverse('posts-views-counter', kwargs={'pk': self.post1.pk})
         self.views2_url = reverse('posts-views-counter', kwargs={'pk': self.post2.pk})
-        self.token1 = self.user1.get_jwt_token_for_user()
-        self.token2 = self.user2.get_jwt_token_for_user()
 
-    def test_list(self):
+    def test_list_posts(self):
         """test: get all posts"""
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 4)
 
-    def test_detail(self):
+    def test_detail_posts(self):
         """test: get post1 by id"""
         response = self.client.get(self.post1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertDictEqual(response.data, PostSerializer(self.post1).data)
 
-    def test_create(self):
+    def test_create_posts(self):
         """test: creating post as user1"""
         self.user1.in_test_api_auth(self.client, self.token1)
         data = {'user': 1, 'title': 'some_title', 'content': 'some_content'}
@@ -74,7 +45,7 @@ class PostViewSetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 3)
 
-    def test_update(self):
+    def test_update_posts(self):
         """test: updating user's post"""
         # user1
         self.user1.in_test_api_auth(self.client, self.token1)
@@ -91,7 +62,7 @@ class PostViewSetTests(APITestCase):
         response = self.client.put(self.post1_detail_url, data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_delete(self):
+    def test_delete_posts(self):
         """test: deleting user's post"""
         # user2
         self.user2.in_test_api_auth(self.client, self.token2)
@@ -105,7 +76,7 @@ class PostViewSetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Post.objects.count(), 1)
 
-    def test_get_by_user(self):
+    def test_get_by_user_posts(self):
         """test: get all user's posts"""
         # user1
         response = self.client.get(self.get_by_user1_url)
@@ -117,7 +88,7 @@ class PostViewSetTests(APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_like(self):
+    def test_like_posts(self):
         """test: post liking"""
         # user1
         self.user1.in_test_api_auth(self.client, self.token1)
@@ -134,7 +105,7 @@ class PostViewSetTests(APITestCase):
         self.assertIn(self.user1, self.post1.liked_by.all())
         self.assertIn(self.user2, self.post1.liked_by.all())
 
-    def test_views_counter(self):
+    def test_views_counter_posts(self):
         """test: post viewing"""
         # user1
         self.user1.in_test_api_auth(self.client, self.token1)
@@ -149,4 +120,3 @@ class PostViewSetTests(APITestCase):
 
         self.assertEqual(self.post1.views, 1)
         self.assertEqual(self.post2.views, 1)
-
